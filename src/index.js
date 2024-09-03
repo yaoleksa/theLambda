7cf1b3733c526d5b7bb5b0f1b35ce3be6fd6d29d
yaoleksa <mryaremchyk@gmail.com>
2024-09-03 14:43:18 +0300

    improve

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
+import ReactDOM from 'react-dom/client';
+import React from 'react';
 import Auth from './auth';
 
-const btn = document.createElement('button');
-btn.innerText = 'Click me';
-btn.addEventListener('click', () => {
-    const auth = new Auth();
-    auth.signUp('doom@org.com', 'xxx');
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-document.getElementById('root').appendChild(btn);
+function LoginPage() {
+    return (<>
+        <div id="welcome_widget">
+            <img src="./imgs/lambda.png" />
+            <p id="moto">New era of communication</p>
+            <div id='login_form'>
+                <div className='login_buttons'>
+                    <p>SignUp</p>
+                </div>
+                <div className='login_buttons'>
+                    <p>LogIn</p>
+                </div>
+            </div>
+        </div>
+    </>);
+}
+
+root.render(<LoginPage />);
